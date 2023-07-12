20f7f7da82fa16b0a1327b757399697fd2533824
leshaluffscats <alexanikeenko317@gmail.com>
2023-07-12 16:37:55 +0300

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,15 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
-    es2021: true,
+    amd: true,
+    node: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
+  globals: {
+    window: true,
+    module: true,
+  },
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
   plugins: ['@typescript-eslint'],
-  rules: {},
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 };
